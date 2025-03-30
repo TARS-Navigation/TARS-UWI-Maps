e59325499fb0db18b7e7506980903db36be31d4b
Richard Leonard <richardleonard4505@gmail.com>
2025-03-30 03:18:15 -0400

    Completed draft models

--- App/models/admin_marker.py
+++ App/models/admin_marker.py
@@ -1,1 +1,22 @@
 from App.database import db
+
+
+class AdminMarker(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    latitude = db.Column(db.Float, nullable = False)
+    longitude = db.Column(db.Float, nullable = False)
+    icon = db.Column(db.String, nullable = False)
+
+    def __init__(self, latitude, longitude, icon):
+        self.latitude = latitude
+        self.longitude = longitude
+        self.icon = icon
+
+
+    def get_json(self):
+        return{
+            'id': self.id,
+            'latitude': self.latitude,
+            'longitude': self.longitude,
+            'icon':self.icon
+        }
--- App/models/user_marker.py
+++ App/models/user_marker.py
@@ -1,1 +1,35 @@
 from App.database import db
+
+class Marker(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    creator_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
+    parent_id = db.Column(db.Integer, db.ForeignKey('marker.id'), nullable = True)
+    latitude = db.Column(db.Float, nullable = False, unique = False )
+    longitude = db.Column(db.Float, nullable = False, unique = False)
+    icon = db.Column(db.String(200), nullable = False)
+    globalVisibility = db.Column(db.Boolean, nullable = False, default = False)
+
+    def __init__(self, creator_id, parent_id, latitude, longitude, icon, globalVisibility
+):
+        self.creator_id = creator_id
+        self.parent_id = parent_id
+        self.latitude = latitude
+        self.longitude = longitude
+        self.icon = icon
+        self.globalVisibility = globalVisibility
+
+    def get_json(self):
+        return {
+        'id': self.id,
+        'creator_id': self.creator_id,
+        'parent_id': self.parent_id,
+        'latitude': self.latitude,
+        'longitude': self.longitude,
+        'icon': self.icon,
+        'globalVisibility': self.globalVisibility
+    }
+        
+    def getChildMarkers(self):
+         child_markers = Marker.query.filter_by(parent_id=self.id).all()
+         return [marker.get_json() for marker in child_markers]
+

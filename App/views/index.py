aaf3617d20c4ec1329fffea8db8d74cb81e470fd
Arvesh Gosine <arveshgosine@gmail.com>
2025-03-31 21:10:34 -0400

    added mockup login and signup ui

--- App/views/index.py
+++ App/views/index.py
@@ -3,9 +3,15 @@ from App.controllers import create_user, initialize
 
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
+#This route is for testing, it can change
 @index_views.route('/', methods=['GET'])
-def index_page():
+def login_page():
     return render_template('login.html')
+
+#This route is for testing, it can change
+@index_views.route('/app', methods=['GET'])
+def index_page():
+    return render_template('index.html')
 
 @index_views.route('/init', methods=['GET'])
 def init():

3331a073264341153d1e3ea2e584768b7c59dd76
Arvesh Gosine <arveshgosine@gmail.com>
2025-04-09 02:38:15 -0400

    Added Map with Leaflet Js

--- App/static/frontend/src/App.js
+++ App/static/frontend/src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useMap } from "react-leaflet";
 
 function App() {
   return (
-    <div>
-      this is working
+    <div style={{ height: "100vh" }}>
+      <MapContainer
+        center={[10.64179, -61.400861]}
+        zoom={17}
+        minZoom={17}
+        maxZoom={18}
+        scrollWheelZoom={false}
+        style={{ height: "100%", width: "100%" }}
+        maxBounds={[
+          [10.63716, -61.39418],
+          [10.64829, -61.40707],
+        ]}
+        maxBoundsViscosity={1.0}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </div>
   );
 }
--- App/views/index.py
+++ App/views/index.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, redirect, render_template, request, send_from_directory, jsonify
 from App.controllers import create_user, initialize
 
+import json, os
+
 index_views = Blueprint('index_views', __name__, template_folder='../templates')
 
 #This route is for testing, it can change
@@ -11,7 +13,23 @@ def login_page():
 #This route is for testing, it can change
 @index_views.route('/app', methods=['GET'])
 def index_page():
-    return render_template('index.html')
+    
+    #DONT CHANGE THIS BLOCK
+    #---------------------------------------------
+    base_dir = os.path.abspath(os.path.dirname(__file__))  # path to this file
+    manifest_path = os.path.join(base_dir, "..", "static", "frontend", "build", "asset-manifest.json") #path to manifest file
+
+    # Normalize the path
+    manifest_path = os.path.normpath(manifest_path)
+
+    with open(manifest_path) as f:
+        manifest = json.load(f)
+
+    main_js = manifest["files"]["main.js"]
+    main_js = "../static/frontend/build/"+ main_js
+    #---------------------------------------------
+
+    return render_template("index.html", main_js=main_js)
 
 @index_views.route('/init', methods=['GET'])
 def init():

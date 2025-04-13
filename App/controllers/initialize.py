687887836a53ea3526a6ffd1dc6219b99b5ab296
Tristan Kalloo <70345555+canaabi@users.noreply.github.com>
2025-04-13 22:19:55 +0000

    Login/Signup

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -1,8 +1,12 @@
 from .user import create_user
+from App.models import Admin
 from App.database import db
-
 
 def initialize():
     db.drop_all()
     db.create_all()
-    create_user('bob', 'bobpass')
+    # Create admin user
+    admin = Admin(username='bob', password='bobpass') # login admin using bob and bobpass
+    admin.admin_id = 1  #  admin ID
+    db.session.add(admin)
+    db.session.commit()
--- App/views/auth.py
+++ App/views/auth.py
@@ -1,69 +1,104 @@
 from flask import Blueprint, render_template, jsonify, request, flash, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user, unset_jwt_cookies, set_access_cookies
+from App.models import Admin
+from App.controllers import get_user_by_username
 
+from .index import index_views
 
-from.index import index_views
-
-from App.controllers import (
-    login
-)
+from App.controllers import (login)
 
 auth_views = Blueprint('auth_views', __name__, template_folder='../templates')
-
-
-
-
 '''
 Page/Action Routes
-'''    
-@auth_views.route('/users', methods=['GET'])
-def get_user_page():
+'''
+
+
+@auth_views.route('/users', methods=['GET', 'POST'])
+def user_page():
+    if request.method == 'POST':
+        data = request.form
+        if not data['username'] or not data['password']:
+            flash('Missing required fields')
+            return redirect(url_for('index_views.index_page')), 400
+
+        user = get_user_by_username(data['username'])
+        if user:
+            flash('Username already exists')
+            return redirect(url_for('index_views.index_page')), 400
+
+        user = create_user(data['username'], data['password'])
+        if user:
+            flash('User created successfully!')
+            return redirect(url_for('auth_views.login_action'))
+        return redirect(url_for('index_views.index_page')), 400
+
     users = get_all_users()
     return render_template('users.html', users=users)
+
 
 @auth_views.route('/identify', methods=['GET'])
 @jwt_required()
 def identify_page():
-    return render_template('message.html', title="Identify", message=f"You are logged in as {current_user.id} - {current_user.username}")
-    
+    return render_template(
+        'message.html',
+        title="Identify",
+        message=
+        f"You are logged in as {current_user.id} - {current_user.username}")
+
 
 @auth_views.route('/login', methods=['POST'])
 def login_action():
     data = request.form
     token = login(data['username'], data['password'])
-    response = redirect(request.referrer)
     if not token:
-        flash('Bad username or password given'), 401
+        flash('Bad username or password given')
+        return redirect(url_for('index_views.index_page')), 401
+
+    user = get_user_by_username(data['username'])
+    flash('Login Successful')
+    response = None
+
+    if isinstance(user, Admin):
+        response = redirect(url_for('admin.index'))
     else:
-        flash('Login Successful')
-        set_access_cookies(response, token) 
+        response = redirect(url_for('index_views.index_page'))
+
+    set_access_cookies(response, token)
     return response
+
 
 @auth_views.route('/logout', methods=['GET'])
 def logout_action():
-    response = redirect(request.referrer) 
+    response = redirect(request.referrer)
     flash("Logged Out!")
     unset_jwt_cookies(response)
     return response
+
 
 '''
 API Routes
 '''
 
+
 @auth_views.route('/api/login', methods=['POST'])
 def user_login_api():
-  data = request.json
-  token = login(data['username'], data['password'])
-  if not token:
-    return jsonify(message='bad username or password given'), 401
-  response = jsonify(access_token=token) 
-  set_access_cookies(response, token)
-  return response
+    data = request.json
+    token = login(data['username'], data['password'])
+    if not token:
+        return jsonify(message='bad username or password given'), 401
+    response = jsonify(access_token=token)
+    set_access_cookies(response, token)
+    return response
+
 
 @auth_views.route('/api/identify', methods=['GET'])
 @jwt_required()
 def identify_user():
-    return jsonify({'message': f"username: {current_user.username}, id : {current_user.id}"})
+    return jsonify({
+        'message':
+        f"username: {current_user.username}, id : {current_user.id}"
+    })
+
 
 @auth_views.route('/api/logout', methods=['GET'])
 def logout_api():

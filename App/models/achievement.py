d7ea1b2c4a76a5fff4eedfffb105a0659c7ceee8
Richard <richardleonard4505@gmail.com>
2025-04-13 20:53:08 -0400

    Added achievements models

---
+++ App/models/achievement.py
@@ -0,0 +1,10 @@
+from App.database import db
+from App.models import Marker
+
+class Achievement(db.Model):
+    __tablename__ = 'achievement'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), nullable = False, unique = False)
+    description = db.Column(db.String(100), nullable = False, unique = False) #description can be anything - for example  Studious (name) - Visited the library (description) 
+    icon = db.Column(db.String(200), nullable = False, unique = False) # each should have an icon, to match map ui and maybe pixelated to give a "game feel"
+    marker_id = db.Column(db.Integer, db.ForeignKey('marker.id')) #some markers that give achievements, not all.  eg food court, library, fst
---
+++ App/models/user_achievements.py
@@ -0,0 +1,20 @@
+from App.database import db
+from App.models import Marker
+from App.models.regular_user import RegularUser
+
+class UserAchievements(db.Model):
+    __tablename__ = 'user_achievements'
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('regular_user.id'), nullable = False)
+    achievement_id = db.Column(db.Integer, db.ForeignKey('achievement.id'), nullable = False)
+    visited = db.Column(db.Boolean, default=False)
+
+    user = db.relationship('RegularUser', backref='user_achievements')
+    achievement = db.relationship('Achievement', backref='user_achievements')
+
+    def get_json(self):
+     return {
+        'id': self.id,
+        'achievement': self.achievement.name,
+        'visited': self.visited
+     }
